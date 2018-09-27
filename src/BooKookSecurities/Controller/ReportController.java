6fe3804457f00ef11e4e1cb6503cc012b00f1976
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-09-27 21:28:21 +0900

    report window binded

--- src/BooKookSecurities/Controller/ReportController.java
+++ src/BooKookSecurities/Controller/ReportController.java
@@ -1,4 +1,28 @@
 package BooKookSecurities.Controller;
 
-public class ReportController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportController implements Initializable {
+    @FXML
+    private TableView table_report;
+    @FXML
+    private Label label_itemSelected;
+
+    public void OnCancelClicked(){
+        System.out.println("cancel clicked");
+    }
+    public void OnDeleteClicked(){
+        System.out.println("delete clicked");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println(label_itemSelected.getText());
+    }
 }
