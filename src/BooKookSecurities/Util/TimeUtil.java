89b402048fcd0b5dd729b3e548ac1097b262fecb
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-11-16 03:52:42 +0900

    time util update

--- src/BooKookSecurities/Util/TimeUtil.java
+++ src/BooKookSecurities/Util/TimeUtil.java
@@ -1,4 +1,17 @@
 package BooKookSecurities.Util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class TimeUtil {
+    public static String getCurrentTime(){
+        Date date = Calendar.getInstance().getTime();
+        String strDateFormat = "hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+
+        String formattedDate = dateFormat.format(date);
+        return formattedDate;
+    }
 }
