63658ccdbff9c6cd394d12eb527b6fa7082588cf
LJihyun <wlgus5781@gmail.com>
2018-12-07 16:03:41 +0900

    Update ExcelManager.java

--- src/BooKookSecurities/Manager/ExcelManager.java
+++ src/BooKookSecurities/Manager/ExcelManager.java
@@ -227,10 +227,10 @@ public class ExcelManager {
             cell.setCellValue(excelInputs.get(idx).getTargetValue());
 
             cell = row.createCell(startCol++);
-            cell.setCellValue(String.format("%.1f", value.average));
+            cell.setCellValue(parseDouble(String.format("%.1f", value.average)));
 
             cell = row.createCell(startCol++);
-            cell.setCellValue(String.format("%.1f", value.max));
+            cell.setCellValue(parseDouble(String.format("%.1f", value.max)));
 
             startCol = base;
             startRow++;
